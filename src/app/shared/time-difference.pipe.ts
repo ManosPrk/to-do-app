1ac38695a1ef21f637bf98813b6e946a8a2ef0a1
manosprk <manosprk@gmail.com>
2021-09-17 16:47:44 +0300

    refactor timeDifference pipe

--- src/app/shared/time-difference.pipe.ts
+++ src/app/shared/time-difference.pipe.ts
@@ -11,33 +11,34 @@ export class TimeDifferencePipe implements PipeTransform {
     }
     const dateUpdated = new Date(timestamp);
     const timeNow = new Date();
-    const diffInYears = timeNow.getFullYear() - dateUpdated.getFullYear();
-    const diffInMonths = timeNow.getMonth() - dateUpdated.getMonth();
-    const diffInDays = timeNow.getDay() - dateUpdated.getDay();
-    const diffInHours = timeNow.getHours() - dateUpdated.getHours();
-    const diffInMinutes = timeNow.getMinutes() - dateUpdated.getMinutes();
+    const diffInMilliSeconds = timeNow.getTime() - dateUpdated.getTime();
+    const diffInSeconds = diffInMilliSeconds / 1000;
+    const diffInMinutes = diffInSeconds / 60;
+    const diffInHours = diffInMinutes / 60;
+    const diffInDays = diffInHours / 24;
+    const diffInMonths = diffInDays / (new Date(timeNow.getFullYear(), timeNow.getMonth() + 1, 0).getDate());
+    const diffInYears = diffInMonths / 12;
 
     let diff: number | null;
     let unitToReturn: string;
-
     switch (true) {
-      case diffInYears > 0:
+      case diffInYears > 1:
         diff = diffInYears;
         unitToReturn = 'year';
         break;
-      case diffInMonths > 0:
+      case diffInMonths > 1:
         diff = diffInMonths;
         unitToReturn = 'month';
         break;
-      case diffInDays > 0:
+      case diffInDays > 1:
         diff = diffInDays;
         unitToReturn = 'day';
         break;
-      case diffInHours > 0:
+      case diffInHours > 1:
         diff = diffInHours;
         unitToReturn = 'hour';
         break;
-      case diffInMinutes > 0:
+      case diffInMinutes > 1:
         diff = diffInMinutes;
         unitToReturn = 'minute';
         break;
@@ -46,12 +47,12 @@ export class TimeDifferencePipe implements PipeTransform {
         unitToReturn = 'seconds';
     }
 
-    if (diff !== null && diff > 1) {
+    if (diff !== null && diff > 2) {
       unitToReturn += 's';
     }
 
     return diff
-      ? `${diff} ${unitToReturn} ago`
+      ? `${Math.round(diff)} ${unitToReturn} ago`
       : `a few ${unitToReturn} ago`;
   }
 }
